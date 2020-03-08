90dfee1cbaf5f4ba4772293e04cfdadaa1eb8217
LiYangzhen <1345356001@qq.com>
2020-03-08 21:45:29 +0800

    structure improve

---
+++ src/js/banner_transparent.js
@@ -0,0 +1,8 @@
+window.onscroll = function () {
+    var a = document.documentElement.scrollTop || document.body.scrollTop;     //滚动条y轴上的距离
+    if (a > 100) {
+        document.querySelector(".banner").style.backgroundColor = "#232a34";
+    } else {
+        document.querySelector(".banner").style.backgroundColor = "transparent";
+    }
+};
