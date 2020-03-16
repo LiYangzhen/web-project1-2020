4eeb83b5851be25505e436ab1178c54702b89dfb
LiYangzhen <1345356001@qq.com>
2020-03-16 22:45:05 +0800

    my_favourite in progress

--- src/js/banner_transparent.js
+++ src/js/banner_transparent.js
@@ -1,8 +1,23 @@
 window.onscroll = function () {
     const a = document.documentElement.scrollTop || document.body.scrollTop;     //滚动条y轴上的距离
-    if (a > 100) {
+    let b = location.href.split('/');
+    b = b[b.length - 1];
+
+    if (b.includes("index") && a > 100) {
+        document.querySelector(".banner").style.backgroundColor = "#232a34";
+    }else if (b.includes("my_favourite")){
         document.querySelector(".banner").style.backgroundColor = "#232a34";
     } else {
         document.querySelector(".banner").style.backgroundColor = "transparent";
     }
+
+    if (a > 200) {
+        document.querySelector("#sidebar").style.visibility = "visible";
+    } else {
+        document.querySelector("#sidebar").style.visibility = "hidden";
+    }
+
 };
+
+
+
